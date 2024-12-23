3c8ff448be7987340b6c11c27d260ed7d0ef0573
gabrielperezx <gabrieloctavioperez@gmail.com>
2024-12-23 16:18:06 -0400

    Actualizar diseño de la página de inicio, mejorar estilos del calendario y galería, y eliminar archivo de pruebas HTML.

--- src/js/app.js
+++ src/js/app.js
@@ -8,10 +8,23 @@
         if (document.body.contains(checkCalendario)) {
             const monthYearElement = document.getElementById('monthYear'),
                 datesElement = document.getElementById('dates'),
+                eventsContainer = document.getElementById('eventsContainer'),
                 prevBtn = document.getElementById('prevBtn'),
                 nextBtn = document.getElementById('nextBtn');
 
             let currentDate = new Date();
+            const events = [
+                {
+                    date: '2024-12-25',
+                    title: 'Navidad',
+                    description: 'Celebración de la Navidad',
+                },
+                {
+                    date: '2024-12-31',
+                    title: 'Fin de Año',
+                    description: 'Cena de fin de año con familia',
+                },
+            ];
 
             const updateCalendar = () => {
                 const currentYear = currentDate.getFullYear(),
@@ -42,11 +55,16 @@
 
                 for (let i = 1; i <= totalDays; i++) {
                     const date = new Date(currentYear, currentMonth, i);
+                    const formattedDate = date.toISOString().split('T')[0];
+                    const hasEvent = events.some(
+                        (event) => event.date === formattedDate,
+                    );
                     const activeClass =
                         date.toDateString() === new Date().toDateString()
                             ? 'active'
                             : '';
-                    datesHTML += `<div class="calendar__date ${activeClass}">${i}</div>`;
+                    const eventClass = hasEvent ? 'event' : '';
+                    datesHTML += `<div class="calendar__date ${activeClass} ${eventClass}" data-date="${formattedDate}">${i}</div>`;
                 }
 
                 for (let i = 1; i <= 6 - lastDayIndex; i++) {
@@ -55,6 +73,41 @@
                 }
 
                 datesElement.innerHTML = datesHTML;
+
+                attachDateClickListeners();
+            };
+
+            const attachDateClickListeners = () => {
+                const dateElements =
+                    document.querySelectorAll('.calendar__date');
+                dateElements.forEach((dateElement) => {
+                    dateElement.addEventListener('click', () => {
+                        const selectedDate =
+                            dateElement.getAttribute('data-date');
+                        showEvents(selectedDate);
+                    });
+                });
+            };
+
+            const showEvents = (date) => {
+                const filteredEvents = events.filter(
+                    (event) => event.date === date,
+                );
+                eventsContainer.innerHTML = '';
+
+                if (filteredEvents.length === 0) {
+                    eventsContainer.innerHTML =
+                        '<p class="m-0 fw-bold">No hay eventos para este día.</p>';
+                } else {
+                    filteredEvents.forEach((event) => {
+                        eventsContainer.innerHTML += `
+                        <div class="calendar__event">
+                            <p class="calendar__event-title">${event.date}</p>
+                            <p class="calendar__event-title">${event.title}</p>
+                            <p class="calendar__event-description">${event.description}</p>
+                        </div>`;
+                    });
+                }
             };
 
             prevBtn.addEventListener('click', () => {
@@ -72,7 +125,7 @@
     };
 
     const LightBox = () => {
-        const checkGaleria = document.querySelector('.gallery');
+        const checkGaleria = document.getElementById('lightboxImage');
         if (document.body.contains(checkGaleria)) {
             // JavaScript para manejar la navegación en el Lightbox
             const images = document.querySelectorAll('.gallery img');
@@ -212,23 +265,102 @@
     };
 
     const CarouselSVG = () => {
-        const images = document.querySelectorAll('.carousel-svg__images img');
-        const dots = document.querySelectorAll('.carousel-svg__dots button');
-        let currentIndex = 0;
+        const checkCarouselSVG = document.querySelector('.carousel-svg');
+        if (document.body.contains(checkCarouselSVG)) {
+            const images = document.querySelectorAll(
+                '.carousel-svg__images img',
+            );
+            const dotsContainer = document.querySelector('.carousel-svg__dots');
+            let currentIndex = 0;
+            let autoSlideInterval;
 
-        dots.forEach((dot, index) => {
-            dot.addEventListener('click', () => {
-                currentIndex = index;
-                updateCarousel();
+            // Crear puntos de navegación dinámicamente
+            dotsContainer.innerHTML = '';
+            images.forEach((_, index) => {
+                const dot = document.createElement('button');
+                if (index === 0) dot.classList.add('active');
+                dotsContainer.appendChild(dot);
             });
-        });
 
-        function updateCarousel() {
-            images.forEach((img, index) => {
-                img.classList.toggle('active', index === currentIndex);
+            const dots = dotsContainer.querySelectorAll('button');
+
+            const updateCarousel = () => {
+                images.forEach((img, index) => {
+                    img.classList.toggle('active', index === currentIndex);
+                });
+                dots.forEach((dot) => dot.classList.remove('active'));
+                dots[currentIndex].classList.add('active');
+            };
+
+            const startAutoSlide = () => {
+                autoSlideInterval = setInterval(() => {
+                    currentIndex = (currentIndex + 1) % images.length;
+                    updateCarousel();
+                }, 3000); // Cambia cada 3 segundos
+            };
+
+            const stopAutoSlide = () => {
+                clearInterval(autoSlideInterval);
+            };
+
+            dots.forEach((dot, index) => {
+                dot.addEventListener('click', () => {
+                    currentIndex = index;
+                    updateCarousel();
+                    stopAutoSlide();
+                });
             });
-            dots.forEach((dot) => dot.classList.remove('active'));
-            dots[currentIndex].classList.add('active');
+
+            images.forEach((img) => {
+                img.addEventListener('click', () => {
+                    stopAutoSlide();
+                });
+            });
+
+            startAutoSlide();
+        }
+    };
+
+    const PopUpInfo = () => {
+        const checkInicio = document.getElementById('VentanaInicio');
+        if (document.body.contains(checkInicio)) {
+            const btnPastoral = document.getElementById('btnPastoral');
+            btnPastoral.addEventListener('click', () => {
+                Swal.fire({
+                    imageUrl: './src/assets/Logocolegio.png',
+                    imageHeight: 100,
+                    imageAlt: 'Logo colegio',
+                    html: `
+                        <p class="fw-bold">Pastoral educativa</p>
+                        <a
+                            href="#"
+                            class="btn btn-primary text-white fw-bold rounded-pill w-100 mb-3"
+                            >Club de naciones unidas - Masmum BQTO</a
+                        >
+                        <a
+                            href="#"
+                            class="btn btn-primary text-white fw-bold rounded-pill w-100 mb-3"
+                            >Club de periodismo y redes sociales</a
+                        >
+                        <a
+                            href="#"
+                            class="btn btn-primary text-white fw-bold rounded-pill w-100 mb-3"
+                            >Club de ajedrez</a
+                        >
+                        <a
+                            href="#"
+                            class="btn btn-primary text-white fw-bold rounded-pill w-100 mb-3"
+                            >Club de matematicas</a
+                        >
+                    `,
+                    showCloseButton: true,
+                    showConfirmButton: false,
+                    customClass: {
+                        actions: 'w-100 justify-content-end px-4',
+                        popup: 'rounded-4',
+                    },
+                });
+            });
         }
     };
 
@@ -243,6 +375,7 @@
         // AlertaBienvenido();
         BotonScrollTop();
         CarouselSVG();
+        PopUpInfo();
     });
     MenuEstatico();
 })();
